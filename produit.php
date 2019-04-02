6030df551ec8b4f37da4ef8ae9221db2df344d70
Toinitus <antoinego96@orange.fr>
2019-04-02 16:59:10 +0200

    status 16h58

--- produit.php
+++ produit.php
@@ -12,34 +12,55 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Produit</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<style type="text/css">
+		
+		h5{
+			color: red;
+
+		}
+
+		
+	</style>
 </head>
-<body>
-	<table>
-		<thead>
-			<tr>
-				<th>Titre</th>
-				<th>Image</th>
-				<th>Description</th>
-				<th>Prix HT</th>
-				<th>Prix TTC</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-				
-				foreach ($beerArray as $value) {
-					echo "<tr>
-						<td>".$value[0]."</td>
-						<td><img class='resizeImage' src='".$value[1]."'></td>
-						<td>".$value[2]."</td>
-						<td>".$value[3]."€</td>
-						<td>".round($value[3]*1.20, 2)."€</td>
-					  <tr>";
-				}
-			?>
-		</tbody>
+<body class="container">
+	<header class="row">
+		<h1 class="col-10 offset-1 text-center">Les bières</h1>
+		 <table class="col-md-12">
+			<tbody>
+	</header>
+		<section class="row">
+				<?php foreach ($beerArray as $element) { ?>
+				<div class="col-md-4 -md-1 mb-5">
+					<div class="row box">
+						<!-- HEADER -->
+						<div class="col-12">
+							<h3 class="my-3 text-center text-uppercase"><?php echo $element[0]; ?></h3>
+						</div>
+						<!-- IMG -->
+						<div class="col-12 box-borders">
+							<img class="my-3 mx-auto d-block" src="<?php echo $element[1]; ?>" height="250" />
+						</div>
+						<!-- Description -->
+						<div class="col-12 ">
+							<p class="my-1 text-justify"><?php echo $element[2]; ?></p>
+						</div>
+						<hr>
+						<!-- Prix -->
+						<div class="col-12">
+							<h5 class="my-3 text-center"><?php echo round($element[3]*1.2,2);?>€</h5>
+						</div>
+					</div>
+				</div>
+				<?php } ?>
+			</section>
+				    </ul>
+				</nav>
+			  </section>
+		    </tbody>
 	</table>
 </body>
 </html>
